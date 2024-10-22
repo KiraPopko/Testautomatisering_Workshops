adb8e3900ac350575c2da42df5cdf95877b2d48f
Jensju <jens@modulmedia.se>
2024-10-23 00:06:53 +0200

    Some changes

--- tests/features/step-definitions/fullScreen.js
+++ tests/features/step-definitions/fullScreen.js
@@ -10,9 +10,15 @@ Given('that I have started the game at {string}', async function (url) {
 
  When('I click the full screen button', async function () {
    await this.driver.findElement(By.css('.go-fullscreen')).click();
+   await this.driver.sleep(2000)
 })
 
 Then('I should get a full screen mode', async function () {
   // I cant figure out how to confirm the fullscreen state...
   // return 'pending';
 }); 
+
+
+
+// Exampel code to use if something eventually apear
+// this.driver.wait(until.elementLocated("attach to something"), 1000)
