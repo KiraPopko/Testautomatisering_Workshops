28a1a7ad143731430057e88af18e055753bdbf0a
AlexandraFilipsson <alexandrafilipsson@gmail.com>
2024-10-24 22:27:31 -0500

    changes commonSteps
    gjorde den enklare och kortare

--- tests/features/step-definitions/commonSteps.js
+++ tests/features/step-definitions/commonSteps.js
@@ -2,7 +2,6 @@ import { Given, When, Then } from '@cucumber/cucumber';
 import { By, until } from 'selenium-webdriver';
 import { expect } from 'chai';
 
-// Gemensamma stegdefinitioner
 const url = "http://localhost:3000";
 
 export const openCafe = async function (driver) {
@@ -32,18 +31,13 @@ Given('that I am outside the cafe', async function () {
 });
 
 When('I wait without taking action', async function () {
-  // Simulerar att användaren inte gör något
+  await this.driver.sleep(2000)
 });
 
-// Lägg till stegdefinitioner för hälsa och pengar
 Then('my health should be {int}', async function (expectedHealth) {
-  const healthElement = await this.driver.findElement(By.css('.health .val'));
-  const actualHealth = await healthElement.getText();
-  expect(parseInt(actualHealth)).to.equal(expectedHealth);
+  await checkHealth(this.driver, expectedHealth);
 });
 
 Then('my money should be {int}', async function (expectedMoney) {
-  const moneyElement = await this.driver.findElement(By.css('.money .val'));
-  const actualMoney = await moneyElement.getText();
-  expect(parseInt(actualMoney)).to.equal(expectedMoney);
+  await checkMoney(this.driver, expectedMoney);
 });
