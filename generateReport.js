b73cb12891e19e72126e1aa655353e39804d497c
KiraPopko <kira.popko@gmail.com>
2024-11-20 21:02:42 +0100

    branch update

--- generateReport.js
+++
@@ -1,9 +0,0 @@
-import generator from 'cucumber-html-reporter';
-
-const options = {
-  theme: 'bootstrap',
-  jsonFile: `./tests/results/chrome.json`,
-  output: `./tests/results/chrome.html`
-};
-
-generator.generate(options);
