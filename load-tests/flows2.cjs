cb7dbe23d41b2022b286838269fc1b16c4928466
KiraPopko <kira.popko@gmail.com>
2024-11-28 20:11:22 +0100

    test

--- load-tests/flows2.cjs
+++ load-tests/flows2.cjs
@@ -1,17 +1,5 @@
-/*module.exports = { helloFlow };
 
-async function helloFlow(page) {
-  //
-  // The code below is just a standard Playwright script:
-  //
-  // Go to https://artillery.io/
-  await page.goto('https://artillery.io/');
-  // Click text=Pricing
-  await page.click('text=Pricing');
-  // assert.equal(page.url(), 'https://artillery.io/pro/');
-  // Click text=Sign up
-  await page.click('text=Sign up');
-}*/
+module.exports = { matAffarFlow };
 
  async function matAffarFlow(page) {
   //
@@ -22,9 +10,9 @@ async function helloFlow(page) {
   await page.goto('http://127.0.0.1:4000/');
 
   // Click text=....
-  //await page.click('text=Skafferi');
-  await page.locator('div').filter({ hasText: /^Skafferi\+$/ }).getByRole('button').click();
+  await page.click('text=Skafferi');
+  //await page.locator('div').filter({ hasText: /^Skafferi\+$/ }).getByRole('button').click();
   
 }
 
-module.exports = { matAffarFlow };
+
