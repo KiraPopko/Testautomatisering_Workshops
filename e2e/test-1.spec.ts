aa0e3a44945be0dce6a7643d5fa92d57473edabe
KiraPopko <kira.popko@gmail.com>
2024-11-29 12:00:55 +0100

    test
    rows from record

--- e2e/test-1.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://127.0.0.1:4000/kategori/frukt-och-gront');
-  await page.locator('div').filter({ hasText: /^Skafferi\+$/ }).getByRole('button').click();
-  await page.locator('div').filter({ hasText: /^Oliver & delikatesser\+$/ }).getByRole('button').click();
-  await page.locator('div').filter({ hasText: /^Kryddor & smaksättare\+$/ }).getByRole('button').click();
-  await page.locator('div').filter({ hasText: /^Olja & vinäger\+$/ }).getByRole('button').click();
-});
--- e2e/test-2.spec.ts
+++ e2e/test-2.spec.ts
@@ -1,22 +1,24 @@
+import { test, expect } from '@playwright/test';
 
-module.exports = { matAffarFlow };
-
- async function matAffarFlow(page) {
+test('test', async ({ page }) => {
   await page.goto('http://127.0.0.1:4000/kategori/frukt-och-gront');
   await page.getByRole('link', { name: 'Skafferi' }).click();
   await page.getByRole('link', { name: 'Oliver & delikatesser' }).click();
   await page.getByRole('link', { name: 'Olja & vinäger' }).click();
   await page.getByRole('link', { name: 'Olivolja' }).click();
+  
+  await page.getByRole('link', { name: 'Kryddmix' }).click();
+  
+  await page.getByRole('link', { name: 'Grönsakskonserver' }).click();
   await page.getByRole('link', { name: 'Torra baljväxter' }).click();
   await page.getByText('Torra baljväxter-Fröer & kä').click();
   await page.getByRole('link', { name: 'Fröer & kärnor' }).click();
   await page.getByRole('link', { name: 'Bakning' }).click();
   await page.getByRole('link', { name: 'Baktillbehör' }).click();
+  
   await page.getByRole('link', { name: 'Fryst' }).click();
   await page.getByRole('link', { name: 'Kött & fågel' }).click();
   await page.getByRole('link', { name: 'Fågel', exact: true }).click();
   
- 
-}
-
-
+  await page.getByRole('link', { name: 'Receptfria läkemedel' }).click();
+});
