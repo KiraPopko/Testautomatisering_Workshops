d885aad9e5b84c558ffb8e51e22a5301e858fb9a
KiraPopko <kira.popko@gmail.com>
2024-11-29 14:11:02 +0100

    last version of test

--- e2e/test-1.spec.ts
+++ e2e/test-1.spec.ts
@@ -1,21 +1,6 @@
+import { test, expect } from '@playwright/test';
 
-module.exports = { matAffarFlow };
-
- async function matAffarFlow(page) {
-  /*await page.goto('http://127.0.0.1:4000/kategori/frukt-och-gront');
-  await page.getByRole('link', { name: 'Skafferi' }).click();
-  await page.getByRole('link', { name: 'Oliver & delikatesser' }).click();
-  await page.getByRole('link', { name: 'Olja & vinäger' }).click();
-  await page.getByRole('link', { name: 'Olivolja' }).click();
-  await page.getByRole('link', { name: 'Torra baljväxter' }).click();
-  await page.getByText('Torra baljväxter-Fröer & kä').click();
-  await page.getByRole('link', { name: 'Fröer & kärnor' }).click();
-  await page.getByRole('link', { name: 'Bakning' }).click();
-  await page.getByRole('link', { name: 'Baktillbehör' }).click();
-  await page.getByRole('link', { name: 'Fryst' }).click();
-  await page.getByRole('link', { name: 'Kött & fågel' }).click();
-  await page.getByRole('link', { name: 'Fågel', exact: true }).click();*/
-  
+test('test', async ({ page }) => {
   await page.goto('http://127.0.0.1:4000/kategori/frukt-och-gront');
   await page.getByRole('link', { name: 'Lotter' }).click();
   await page.getByRole('heading', { name: 'Triss Lott' }).click();
@@ -39,6 +24,4 @@ module.exports = { matAffarFlow };
   await page.getByRole('button', { name: '-' }).first().click();
   await page.getByRole('link', { name: 'Fryst' }).click();
   await page.getByRole('link', { name: 'Kött, burgare & korv mm' }).click();
-}
-
-
+});
