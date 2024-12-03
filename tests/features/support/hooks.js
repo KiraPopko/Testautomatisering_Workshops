9fca9cec22315cd22f61682866705502143d017f
AlexandraFilipsson <alexandrafilipsson@gmail.com>
2024-12-03 14:46:13 -0500

    small update

--- tests/features/support/hooks.js
+++ tests/features/support/hooks.js
@@ -3,7 +3,7 @@ import { sleepBetweenSteps } from '../../config.js';
 import { driver } from "./world.js";
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 Before(function () {
-  return this.driver.manage().window().maximize();
+  //return this.driver.manage().window().maximize();
 });
 AfterAll(function () {
   return driver.quit();
