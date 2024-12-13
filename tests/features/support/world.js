7e9fe187b6a8613d57781a69876fa86a25f0260a
AlexandraFilipsson <alexandrafilipsson@gmail.com>
2024-12-13 08:37:40 -0500

    try for git action

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -1,18 +1,43 @@
+// import seleniumWebdriver from 'selenium-webdriver';
+// import { setWorldConstructor, setDefaultTimeout } from '@cucumber/cucumber';
+// import { timeout, headless } from '../../config.js';
+// import edge from 'selenium-webdriver/edge.js';
+// const options = new edge.Options();
+// //headless && options.addArguments('--headless=new');
+// export const driver = new seleniumWebdriver
+//   .Builder()
+//   .setEdgeOptions(options)
+//   .forBrowser("MicrosoftEdge")
+//   .build();
+// class CustomWorld {
+//   constructor() {
+//     this.driver = driver;
+//   }
+// }
+// setDefaultTimeout(timeout);
+// setWorldConstructor(CustomWorld);
+
 import seleniumWebdriver from 'selenium-webdriver';
 import { setWorldConstructor, setDefaultTimeout } from '@cucumber/cucumber';
-import { timeout, headless } from '../../config.js';
-import edge from 'selenium-webdriver/edge.js';
-const options = new edge.Options();
-//headless && options.addArguments('--headless=new');
-export const driver = new seleniumWebdriver
-  .Builder()
-  .setEdgeOptions(options)
-  .forBrowser("MicrosoftEdge")
-  .build();
+import { timeout, browser, headless } from '../../config.js';
+import chrome from 'selenium-webdriver/chrome.js';
+
+const GITHUB_ACTIONS = process.env['GITHUB_ACTIONS'] === 'true';
+
+const options = new chrome.Options();
+// Run headless if config says so or if we are running in GitHub actions 
+(headless || GITHUB_ACTIONS) && options.addArguments('--headless=new');
+
+
 class CustomWorld {
   constructor() {
-    this.driver = driver;
+    this.driver = new seleniumWebdriver
+      .Builder()
+      .setChromeOptions(options)
+      .forBrowser(browser)
+      .build();
   }
 }
+
 setDefaultTimeout(timeout);
 setWorldConstructor(CustomWorld);
