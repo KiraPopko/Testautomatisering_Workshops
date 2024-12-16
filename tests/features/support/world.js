1ea4b80d252a6c2c91fe5fa19c8e0f335ef635ac
AlexandraFilipsson <alexandrafilipsson@gmail.com>
2024-12-16 07:35:36 -0500

    git try

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -4,20 +4,36 @@ import { timeout, headless, browser } from '../../config.js';
 import edge from 'selenium-webdriver/edge.js';
 import chrome from 'selenium-webdriver/chrome.js';
 
-const GITHUB_ACTIONS = process.env['GITHUB_ACTIONS'] === 'true';
+let _driver;
 
-const options = new chrome.Options();
-//headless && options.addArguments('--headless=new');
-(headless || GITHUB_ACTIONS) && options.addArguments('--headless=new');
-export const driver = new seleniumWebdriver
-  .Builder()
-  .setEdgeOptions(options)
-  .forBrowser(browser)
-  .build();
+const GITHUB_ACTIONS = process.env['GITHUB_ACTIONS'] === 'true';
+if (!GITHUB_ACTIONS) { //local run edge
+  const options = new edge.Options();
+  //headless && options.addArguments('--headless=new');
+  _driver = new seleniumWebdriver
+    .Builder()
+    .setEdgeOptions(options)
+    .forBrowser("MicrosoftEdge")
+    .build();
+}
+else { //github action run chrome
+  const options = new chrome.Options();
+  options.addArguments('--headless=new');
+  _driver = new seleniumWebdriver
+    .Builder()
+    .setChromeOptions(options)
+    .forBrowser("Chrome")
+    .build();
+}
+
+export const driver = _driver;
+
 class CustomWorld {
   constructor() {
     this.driver = driver;
   }
 }
+
+
 setDefaultTimeout(timeout);
 setWorldConstructor(CustomWorld);
